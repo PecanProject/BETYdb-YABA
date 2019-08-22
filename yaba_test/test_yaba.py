ac1c4ed1a82356fe2d2e9db286155a742ee230fc
saurabh1969 <kumarsaurabh772@gmail.com>
2019-08-22 23:49:26 +0530

    Resolving merge conflicts

---
+++ yaba_test/test_yaba.py
@@ -0,0 +1,107 @@
+# users_test/test_users.py
+import unittest
+import requests
+import time
+
+
+BASE_URL = 'http://0.0.0.0:5001/'
+
+
+class BasicTests(unittest.TestCase):
+
+    def test_insert_experiments(self):
+        csv_filename = 'experiments.csv'
+        csv_path = 'input_files/experiments.csv'
+        files = {'fileName': open(csv_path, 'rb')}
+        response = requests.post(
+            'http://0.0.0.0:5001/yaba/v1/experiments?username=guestuser', files=files)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(response.ok)
+
+    def test_insert_sites(self):
+        csv_filename = 'sites.csv'
+        csv_path = 'input_files/'+csv_filename
+
+        shp_file = 'input_files/S8_two_row_polys.shp'
+        dbf_file = 'input_files/S8_two_row_polys.dbf'
+        prj_file = 'input_files/S8_two_row_polys.prj'
+        shx_file = 'input_files/S8_two_row_polys.shx'
+
+        files= {'fileName': open(csv_path, 'rb'),
+                'shp_file': open(shp_file, 'rb'),
+                'dbf_file':open(dbf_file, 'rb'),
+                'prj_file': open(prj_file, 'rb'),
+                'shx_file': open(shx_file, 'rb')}
+
+        response = requests.post('http://0.0.0.0:5001/yaba/v1/sites',
+                            files=files)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(response.ok)
+
+    def test_insert_treatments(self):
+        csv_filename = 'treatments.csv'
+        csv = 'input_files/'+csv_filename
+        files = {'fileName': open(csv, 'rb')}
+        response = requests.post(
+            'http://0.0.0.0:5001/yaba/v1/treatments?username=guestuser', files=files)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(response.ok)
+
+    def test_insert_cultivars(self):
+        csv_filename = 'cultivars.csv'
+        csv = 'input_files/'+csv_filename
+        files = {'fileName': open(csv, 'rb')}
+        response = requests.post(
+            'http://0.0.0.0:5001/yaba/v1/cultivars', files=files)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(response.ok)
+
+    def test_insert_citations(self):
+        csv_filename = 'citations.csv'
+        csv = 'input_files/'+csv_filename
+        files = {'fileName': open(csv, 'rb')}
+        response = requests.post(
+            'http://0.0.0.0:5001/yaba/v1/citations?username=guestuser', files=files)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(response.ok)
+        
+    def test_insert_experimentSites(self):
+        csv_filename = 'experiments_sites.csv'
+        csv = 'input_files/'+csv_filename
+        files = {'fileName': open(csv, 'rb')}
+        response = requests.post(
+            'http://0.0.0.0:5001/yaba/v1/experiments_sites', files=files)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(response.ok)
+
+    def test_insert_experimentTreatments(self):
+        csv_filename = 'experiments_treatments.csv'
+        csv = 'input_files/'+csv_filename
+        files = {'fileName': open(csv, 'rb')}
+        response = requests.post(
+            'http://0.0.0.0:5001/yaba/v1/experiments_treatments', files=files)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(response.ok)
+
+    def test_insert_sitesCultivars(self):
+        csv_filename = 'sites_cultivars.csv'
+        csv = 'input_files/'+csv_filename
+        files = {'fileName': open(csv, 'rb')}
+        response = requests.post(
+            'http://0.0.0.0:5001/yaba/v1/sites_cultivars', files=files)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(response.ok)
+
+    def test_insert_citationsSites(self):
+        csv_filename = 'citations_sites.csv'
+        csv = 'input_files/'+csv_filename
+        files = {'fileName': open(csv, 'rb')}
+        response = requests.post(
+            'http://0.0.0.0:5001/yaba/v1/citations_sites', files=files)
+        self.assertEqual(response.status_code, 201)
+        self.assertTrue(response.ok)
+
+
+if __name__ == "__main__":
+    time.sleep(1)
+    unittest.main()
