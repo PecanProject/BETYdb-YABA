6037b55a5a8e59efb743a058b8fb1537cf0eab8a
terabo <yashsrivastava85p@gmail.com>
2020-07-26 18:33:51 +0530

    Containerized the Interface and Visualiztion

--- visualization/index.js
+++ visualization/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3001
+const port = 8080
 const { getCultivarSites, getExperimentSites, getTreatmentSites } = require('./pg_joins')
 const { shapeParser }= require('./shape-parser')
 const { sheetToCsv }= require('./sheet-parser')
@@ -13,7 +13,7 @@ const path= require('path')
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
   next();
