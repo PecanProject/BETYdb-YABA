205c6a632a11a62ba39285a544934d8e1e32ba9c
Mansi <116175281+Mansi-dass@users.noreply.github.com>
2023-04-20 20:20:18 +0530

    Create wait-for-server.sh

---
+++ wait-for-server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until curl --silent --output /dev/null --fail "http://$host"; do
+  echo "Waiting for server..."
+  sleep 5
+done
+
+exec $cmd
